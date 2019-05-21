9cf1d82d31c25ce48ad5e2c53ecda629290659ed
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2019-05-21 19:20:48 +0100

    menu mobile settings

--- js/active.js
+++ js/active.js
@@ -214,3 +214,18 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
         }
     }
 });
+
+
+
+window.onscroll = () => {menuScroll()};
+        
+        let stickyMenu = document.getElementById("stickyHeader");
+        let sticky = stickyMenu.offsetTop;
+        
+        function menuScroll() {
+          if (window.pageYOffset >= sticky) {
+            stickyMenu.classList.add("stickyTop")
+          } else {
+            stickyMenu.classList.remove("stickyTop");
+          }
+        }
